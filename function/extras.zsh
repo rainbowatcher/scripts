cd547edf5a9f7eccb3749ee7d49c6c082e4c3825
rainbowatcher <rainbow-w@qq.com>
2022-10-20 22:01:29 +0800

    fix: pip freeze not compatible with outdated option

--- function/extras.zsh
+++ function/extras.zsh
@@ -63,7 +63,7 @@ function global_update() {
   step "update python packages"
   if cmd_exists python && cmd_exists pip; then
     python -m pip install --upgrade pip
-    pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
+    pip list -u --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
   fi
 
   step "update node packages"
