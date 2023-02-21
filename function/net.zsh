16d31657b436cfb0d1ea7123f209d91c124aa1d3
rainbowatcher <rainbow-w@qq.com>
2023-02-21 11:09:33 +0800

    docs: add doc for get state code

--- function/net.zsh
+++ function/net.zsh
@@ -1,3 +1,5 @@
+# Request to a server, And fetch only the HTTP state code from the response
+# $1: request url
 function get_status_code(){
-  curl -LI -m 10 -o /dev/null -s -w %{http_code} $1
+  curl -LI -m 10 -o /dev/null -s -w '%{http_code}' $1
 }
