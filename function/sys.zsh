b51d564ca2b56ffca42bc9f1fa35c8121a995419
rainbowatcher <rainbow-w@qq.com>
2023-03-20 13:19:50 +0800

    chore: switch du* alias to function

--- function/sys.zsh
+++ function/sys.zsh
@@ -18,3 +18,27 @@ function ram() {
     echo "No active processes matching pattern '${BLUE}${app}${FONT}'"
   fi
 }
+
+# display target dir disk usage statistics in humen readable sort
+# $1: target dir
+function dud() {
+  local target=$1
+  du -hd1 $target | sort -h
+}
+
+# display all file disk usage statistics in humen readable sort
+function dua() {
+  local target=$1
+  du -ahd1 $target | sort -h
+}
+
+# display node_modules disk usage statistics in humen readable sort
+function dun() {
+  if test -d "node_modules/.pnpm"; then
+    dud "node_modules/.pnpm"
+  elif test -d "node_modules"; then
+    dud "node_modules"
+  else
+    warn "node_modules not found."
+  fi
+}
