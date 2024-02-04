97f03e640ee196ad053851d5c258f1c493416c31
rainbowatcher <rainbow-w@qq.com>
2024-02-04 09:49:49 +0800

    chore: use trash instead of rm, remove grep and ip alias

--- alias/cli.zsh
+++ alias/cli.zsh
@@ -10,10 +10,11 @@ alias ll='ls -lFh --time-style long-iso'
 alias lg='ls -lbGaFh --time-style long-iso'
 alias lx='ls -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'
 alias zshrc='code ~/.zshrc'
-alias rm='rm -i'
+alias rm='trash'
+# alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-alias grep='rg --color=auto'
+# alias grep='rg --color=auto'
 alias fdd='fd -Ht d'
 alias fdf='fd -Ht f'
 
@@ -25,6 +26,6 @@ alias -s tar.gz="tar -tf"
 alias -s gz="gunzip -l"
 
 # IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+# alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6? (addr:)?\s?((([0-9]+.){3}[0-9]+)|[a-fA-F0-9:]+)' | awk '{sub(/inet6? (addr:)? ?/, \"\"); print}'"
