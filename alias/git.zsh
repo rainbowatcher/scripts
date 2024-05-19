0aa62dfa5b56045eac9532d220870e5cb2854019
rainbowatcher <rainbow-w@qq.com>
2024-05-19 23:20:53 +0800

    feat: add git tag alias

--- alias/git.zsh
+++ alias/git.zsh
@@ -4,18 +4,28 @@ alias gaa='git add .'
 alias gba='git branch -a'
 alias gbd='git branch --delete'
 alias gbrn='git branch -m'
+
 alias gc='git commit -m'
 alias gca='git commit -am'
 alias gcam='git commit --amend --no-edit'
+
 alias gcat='git cat-file'
+
 alias gcf='git config -l'
+
 alias gcl1='git clone --depth 1'
 alias gcl='git clone'
+
 alias gcln='git clean -xdf'
 alias gdf='git difftool'
 alias gho='git hash-object'
+
 alias gl='git log --oneline --cherry'
 alias gll='git log --graph --cherry --pretty=format:"%h <%an> %ar %s"'
+
+alias gtl='git tag -l'
+alias gtd='git tag -d'
+
 alias gp='git push'
 alias gpl='git pull'
 alias gs='git switch'
