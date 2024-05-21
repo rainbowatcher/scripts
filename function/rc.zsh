e87f68a4c5f0a0c7f19afeb2b46fa06bc86f9a88
rainbowatcher <rainbow-w@qq.com>
2024-05-21 11:07:52 +0800

    feat: add zsh options

--- function/rc.zsh
+++ function/rc.zsh
@@ -7,8 +7,28 @@
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
+setopt inc_append_history     # Write to the history file immediately, not when the shell exits.
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_all_dups   # Delete old recorded entry if new entry is a duplicate.
+setopt hist_find_no_dups      # Do not display a line previously found.
 setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_reduce_blanks     # Remove superfluous blanks before recording entry.
+setopt hist_save_no_dups      # Don't write duplicate entries in the history file.
 setopt hist_verify            # show command with history expansion to user before running it
-setopt share_history          # share command history data
+# setopt share_history          # share command history data
+
+
+# Disable correction
+unsetopt correct_all
+unsetopt correct
+DISABLE_CORRECTION="true"
+
+unsetopt share_history
+setopt prompt_subst
+unsetopt prompt_cr prompt_sp
+
+# make zsh can use `#` in interactive shell
+setopt interactivecomments
+# make `?` in string not need for quote
+setopt no_nomatch
