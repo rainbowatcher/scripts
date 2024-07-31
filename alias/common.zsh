e4e407c91c37c841ab5ce39905c3852660168ac9
rainbowatcher <rainbow-w@qq.com>
2024-07-31 15:52:12 +0800

    feat: add command correction

--- alias/common.zsh
+++ alias/common.zsh
@@ -45,3 +45,11 @@ alias -s gz="gunzip -l"
 alias public_ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias local_ip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -oE 'inet6? (addr:)?\s?((([0-9]+.){3}[0-9]+)|[a-fA-F0-9:]+)' | awk '{sub(/inet6? (addr:)? ?/, \"\"); print}'"
+
+# CORRECTION
+alias cp="nocorrect cp"
+alias man="nocorrect man"
+alias mkdir="nocorrect mkdir"
+alias mv="nocorrect mv"
+alias sudo="nocorrect sudo"
+alias su="nocorrect su"
