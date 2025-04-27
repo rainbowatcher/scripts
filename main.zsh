53956ba94ca8b3e7d1a476684cae342ff133570e
rainbowatcher <rainbow-w@qq.com>
2025-04-27 12:12:19 +0800

    feat: load script use standard way

--- main.zsh
+++ main.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
+# ref: https://github.com/zdharma-continuum/fast-syntax-highlighting/blob/master/fast-syntax-highlighting.plugin.zsh#L37
+# Standarized way of handling finding plugin dir,
+# regardless of functionargzero and posixargzero,
+# and with an option for a plugin manager to alter
+# the plugin directory (i.e. set ZERO parameter)
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
 FONT='\033[0m'
 GREEN="\033[1;32m"
 RED='\033[1;31m'
@@ -8,14 +17,14 @@ BLUE='\033[1;36m'
 
 # add folder in functions to fpath
 fpath=(
-    "$SCRIPTS_ROOT"/functions/**/*(N/)
+    "${0:h}"/functions/**/*(N/)
     $fpath
 )
 
 # load functions
-for func_file in "$SCRIPTS_ROOT"/{functions,completions,alias}/**/*(.); do
+for func_file in "${0:h}"/{functions,completions,alias}/**/*(.); do
     if [[ $func_file =~ /functions/ ]]; then
-        autoload -Uz "${func_file:t}"; 
+        autoload -Uz "${func_file:t}"
     else
         zsh-defer source "${func_file}"
     fi
